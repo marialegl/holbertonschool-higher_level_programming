944c819382c04afa943514893c5fc60d44c2be2b
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-19 23:55:05 +0000

    Python - Classes and Objects

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """Square with size"""
+    def __init__(self, size=0):
+        """Constructor of square object
+        Size must  be a positive integer"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
