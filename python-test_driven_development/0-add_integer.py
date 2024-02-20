2e6cf75253058bea2a27a3024e10dc1a6fabc71b
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-20 13:54:50 +0000

    funFion that adds 2 integers and tests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to add 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+    
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
