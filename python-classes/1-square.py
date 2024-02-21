b771691c6b3c21670b476b5bac6a06494142948e
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-21 16:11:59 +0000

    Changes of the code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Square Class
+
+This class represents a square.
+"""
+
+
 class Square:
-    """Square with size"""
+    """
+    Square with size
+    args= size
+    """
     def __init__(self, size):
         self.__size = size
