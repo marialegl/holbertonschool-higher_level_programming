2ff6f7c85a1b9a7199c99889c170d37ac9787ba7
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-21 15:58:33 +0000

    Changes of the code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     item = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end=" ")
+            print("{:d}".format(my_list[i]), end="")
             item += 1
         except (ValueError, TypeError):
             continue
-    print("")
+    print()
     return (item)
