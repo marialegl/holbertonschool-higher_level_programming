9e58fd9d675064b3a67c1701bab8b1b72d006bf4
marialegl <psico.marialegl@gmail.com>
2024-02-25 18:04:39 -0500

    function that returns the list of available attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Methods of an object
+
+Defines an object attribute lookup function.
+"""
+
+
+def lookup(obj):
+    """
+    Return a list of an object's available attributes.
+    """
+    return (dir(obj))
