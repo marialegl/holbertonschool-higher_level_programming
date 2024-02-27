03276f302ca675a3694c246b8c0913e6872cbeff
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-27 17:33:56 +0000

    Function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Module 
-to add 
-2 integers
-123
+"""Function that adds 2 integers.
+a and b must be first casted to integers if they are float
+a and b must be integers or floats, otherwise raise a TypeError
+Returns an integer: the addition of a and b
 """
+
+
 def add_integer(a, b=98):
     """
-    comentar
+    Function that adds 2 integers.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or float")
-    
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-
     return a + b
--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
-print(add_integer("hello", "como estas"))
+print(add_integer(-1, 0))
 print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
