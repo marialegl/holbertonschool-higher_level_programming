ec43c0408e1b595e5eb4e54c52741c28362f2450
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-27 20:49:19 +0000

    Function that reads a text file (UTF8), and prints it to std

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Function that reads a text file (UTF8)
+and prints it to stdout
+Using the "with" statement.
+"""
+
+
 def read_file(filename=""):
+    """
+    Function that reads a text file
+    """
     with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
+        print(file.read(), end="")
