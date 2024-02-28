63452cf456020198e12b0efbc71730f2c357fa22
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-28 03:57:37 +0000

    Function that returns the dictionary description with simple data structure

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -11,8 +11,4 @@ def class_to_json(obj):
     Function that returns the dictionary description
     with simple data structure
     """
-    description = {}
-    for attr_name in dir(obj):
-        attr_value = getattr(obj, attr_name)
-        if isinstance(attr_value, (list, dict, str, int, bool)):
-            description[attr_name] = attr_value
+    return obj.__dict__
