d69a3abde71ce331c0213a61149dc50dd0a5366b
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-28 20:51:56 +0000

    script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,15 @@ and then save them to a file
 Using the "with" statement
 """
 
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-import json
+filename = "add_item.json"
 
+try:
+    new_list = load_from_json_file(filename)
+except Exception:
+    new_list = []
 
-def save_to_json_file(my_obj, filename):
-    """
-    Script that adds all arguments to a Python list
-    and then save them to a file
-    """
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
+save_to_json_file(new_list + argv[1:], filename)
