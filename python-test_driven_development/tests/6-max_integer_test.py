a18da68c2e528f3aaa7f0140ca9b670310107e21
marialegl <148149112+marialegl@users.noreply.github.com>
2024-02-29 00:54:22 +0000

    Changes in the tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,33 +1,23 @@
-#!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unittest.TestCase):
-    """
-    A class to test a max integer function
-    """
+class TestMaxIntegers(unittest.TestCase):
 
     def test_max_integer(self):
-        """
-        Test the max integer in a list of integers when the integers
-        are positive or negative numbers
-        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([7]), 7)
+        self.assertEqual(max_integer([1, 2, 3, 2]), 3)
+
+    def test_not_parmts(self):
         self.assertIsNone(max_integer([]))
-        self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
-        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
-        self.assertAlmostEqual(max_integer([1.0, 1.5, 1.6, 3.7, 2.3]), 3.7)
-        self.assertAlmostEqual(max_integer([7.7]), 7.7)
+        self.assertIsNone(max_integer())
 
-    def test_wrong_types(self):
-        """
-        Test the max integer with wrong parameters types
-        """
-        with self.assertRaises(TypeError):
-            max_integer(None)
+    def testIntNegative(self):
+        self.assertEqual(max_integer([-2, -1, 0, 1]), 1)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
-        with self.assertRaises(TypeError):
-            max_integer(["Monty", 89, 34, -9.7, "Python"])
+
+if __name__ == '__main__':
+    unittest.main()
