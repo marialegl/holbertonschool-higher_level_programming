3454a554b524fb97d5965c54e648033fc0464788
marialegl <148149112+marialegl@users.noreply.github.com>
2024-03-01 21:25:26 +0000

    Updated the public method def update: changing the prototype to update(self, *args, **kwargs), If args exists and is not empty, updates the attributes according to the order of the arguments and If args is empty or does not exist, update attributes according to kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,15 +96,23 @@ class Rectangle(Base):
         return (f'[Rectangle] ({self.id}) {self.x}/{self.y} - '
                 f'{self.width}/{self.height}')
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns an argument to each attribute: id, width, height, x and y"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args and len(args) > 0:
+            """If args exists and is not empty, updates the attributes
+            according to the order of the arguments."""
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            """If args is empty or does not exist, update
+            attributes according to kwargs"""
+            for key, value in kwargs.items():
+                setattr(self, key, value)
