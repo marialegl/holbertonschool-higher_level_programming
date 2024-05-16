2e726e46bd74bf08514ffdf49f06474992ef569d
marialegl <psico.marialegl@gmail.com>
2024-05-15 22:30:03 -0500

    Task 12 Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+    ans = 0
+    n_len = len(roman_string)
+    for (i, v) in enumerate(roman_string):
+        if i < n_len - 1 and rom[v] < rom[roman_string[i + 1]]:
+            ans -= rom[v]
+        else:
+            ans += rom[v]
+    return ans
