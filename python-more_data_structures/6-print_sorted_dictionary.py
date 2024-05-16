2a072e1b13d49783b5b5197748a8d6919dfa00e2
marialegl <psico.marialegl@gmail.com>
2024-05-15 20:27:36 -0500

    Task 6  function that prints a dictionary by ordered keys.

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+def print_sorted_dictionary(a_dictionary):
+    # Ordenar las claves del diccionario alfabéticamente
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Imprimir el diccionario ordenado por las claves
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
