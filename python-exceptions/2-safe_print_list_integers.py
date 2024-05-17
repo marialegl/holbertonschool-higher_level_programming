e418f7b2e0388d8465b042e36bf10914b4666c29
marialegl <psico.marialegl@gmail.com>
2024-05-17 06:39:36 -0500

    Project Exceptions Tasks

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def safe_print_list_integers(my_list=[], x=0):
+    item = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            item += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return (item)
--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def raise_exception():
+    try:
+        x = 5 + "Hello"
+    except TypeError as e:
+        raise e
