c121bb2e02de43b342cb12ac763414aaee977631
marialegl <psico.marialegl@gmail.com>
2024-05-28 12:10:32 -0500

    Update task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -14,6 +14,8 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be positive")
         self.radius = radius
 
     def area(self):
