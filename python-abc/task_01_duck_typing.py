e5634b52492831abe99035a470226538f5325aff
marialegl <psico.marialegl@gmail.com>
2024-05-28 11:57:52 -0500

    Task 1 Create an abstract class named Shape with two abstract methods: area and perimeter.

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius=radius
+
+    def area(self):
+        return (3.14159*self.radius*self.radius)
+
+    def perimeter(self):
+        return (2*3.14159*self.radius)
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width=width
+        self.height=height
+
+    def area(self):
+        return (self.width*self.height)
+
+    def perimeter(self):
+        return (2*self.width+2*self.height)
+
+def shape_info(Shape):
+    print("Area:", Shape.area())
+    print("Petimeter:", Shape.perimeter())
