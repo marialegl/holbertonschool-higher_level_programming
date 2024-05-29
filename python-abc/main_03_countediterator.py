fde8807ef9cee5f6e2988be1136c1a08c6b060de
marialegl <psico.marialegl@gmail.com>
2024-05-28 20:42:06 -0500

    task 3 Create a class named CountedIterator that extends the built-in iterator obtained from the iter function.

--- python-abc/main_03_countediterator.py
+++ python-abc/main_03_countediterator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from task_03_countediterator import CountedIterator
 
 data = [1, 2, 3, 4]
--- python-abc/main_04_flyingfish.py
+++ python-abc/main_04_flyingfish.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from task_04_flyingfish import Fish, FlyingFish
 
 flying_fish = FlyingFish()
