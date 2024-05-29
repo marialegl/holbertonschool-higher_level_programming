2358c131ea1ea7279b13a809f2fc5dbfa4d74173
marialegl <psico.marialegl@gmail.com>
2024-05-28 22:20:34 -0500

    Task 4

--- python-abc/main_04_flyingfish.py
+++ python-abc/main_04_flyingfish.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from task_04_flyingfish import Fish, FlyingFish
+from task_04_flyingfish import Bird
+from task_04_flyingfish import FlyingFish
 
-flying_fish = FlyingFish()
+flying_fish = FlyingFish("underwater", "in the air")
 flying_fish.swim()
 flying_fish.fly()
 flying_fish.habitat()
