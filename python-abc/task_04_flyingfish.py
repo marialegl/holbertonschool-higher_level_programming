842f9d6f8cc0072a37c3f6e077eb48986bfedc5d
marialegl <psico.marialegl@gmail.com>
2024-05-28 22:25:01 -0500

    Task 5

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -25,13 +25,10 @@ class FlyingFish(Fish, Bird):
         print("The flying fish lives both in water and the sky!")
 
 
-# Instantiate an object of the FlyingFish class
 flying_fish = FlyingFish()
 
-# Call the fly, swim, and habitat methods and observe the outputs
 flying_fish.swim()
 flying_fish.fly()
 flying_fish.habitat()
 
-# Investigate the method resolution order
 print(FlyingFish.mro())
