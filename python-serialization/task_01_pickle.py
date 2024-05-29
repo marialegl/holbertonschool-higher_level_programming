2c5d2007880e251894ebf2959bad498aa4f8d7c9
marialegl <psico.marialegl@gmail.com>
2024-05-29 12:32:41 -0500

    Taks 1 Serialize and deserialize custom Python objects using the pickle module.

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Serialize and deserialize custom Python
+objects using the pickle module.
+"""
+
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except (IOError, pickle.PickleError) as e:
+            print(f"Error serializing object: {e}")
+    
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return (pickle.load(file))
+        except (IOError, pickle.PickleError) as e:
+            print(f"Error deserializing object: {e}")
+            return (None)
+
+    def display(self):
+        print("Name: {}\nAge: {}\nIs Student: {}". format(
+            self.name, self.age, self.is_student))
