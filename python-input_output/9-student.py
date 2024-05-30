925d6ed606b4d108c7e69a192884ebbced70a6ba
marialegl <psico.marialegl@gmail.com>
2024-05-29 21:35:42 -0500

    Task 9 Student class module

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Student class module
+"""
+
+
+class Student:
+    """ Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Returns a dictionary representation of a Student instance."""
+        return self.__dict__
