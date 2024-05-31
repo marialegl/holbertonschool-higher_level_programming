5ab9a2816090a0c5b4e587229b7bdfd96d044c98
marialegl <psico.marialegl@gmail.com>
2024-05-31 10:14:55 -0500

    Update

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class MiRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Endpoint not found")
+            self.wfile.write(b"404 Not Found")
             
 
 
