f5c07260dab0b806296ae53ba5ca180ea6553b0e
marialegl <psico.marialegl@gmail.com>
2024-05-30 19:11:49 -0500

    Update Task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,13 +8,11 @@ import requests
 import csv
 
 
-url = 'https://jsonplaceholder.typicode.com/posts'
-response = requests.get(url)
-
-
 def fetch_and_print_posts():
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
