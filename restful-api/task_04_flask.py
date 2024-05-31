949c2397d8b1535b26ee889e079f5a43a94e4d68
marialegl <psico.marialegl@gmail.com>
2024-05-31 16:45:52 -0500

    Task 5

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,23 +6,28 @@ Handling and response formation in Flask.
 Handle POST requests to add new data to the API.
 """
 
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 users = {}
 
+
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route('/data')
 def get_users():
     return jsonify(list(users.keys()))
 
+
 @app.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -32,26 +37,16 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    if not data or 'username' not in data:
-        return jsonify({"error": "Bad request, 'username' is required"}), 400
-    
-    username = data['username']
-    if username in users:
-        return jsonify({"error": "User already exists"}), 400
-    
-    if 'name' not in data or 'age' not in data or 'city' not in data:
-        return jsonify({"error": "Bad request, 'name', 'age', and 'city' are required"}), 400
-    
-    users[username] = {
-        "name": data["name"],
-        "age": data["age"],
-        "city": data["city"]
-    }
 
-    return jsonify({"message": "User added", "user": users[username]}), 201
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    user_to_add = request.get_json()
+    username = user_to_add.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_to_add
+    return jsonify({"message": "User added", "user": user_to_add}), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
