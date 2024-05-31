4be4272825c1fc3f165c3ea4f6477e5e1a377d81
marialegl <psico.marialegl@gmail.com>
2024-05-31 15:12:57 -0500

    update

--- restful-api/postdata.py
+++ restful-api/postdata.py
@@ -11,3 +11,5 @@ data = {
 
 resp = requests.post('http://localhost:5000/add_user', json=data)
 print (resp)
+
+host='localhost', port=5000, 
--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -54,4 +54,4 @@ def add_user():
     return jsonify({"message": "User added", "user": users[username]}), 201
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True)
