2530a3a277f3d36f675eae41956437d88fdc1893
marialegl <psico.marialegl@gmail.com>
2024-05-31 10:59:21 -0500

    Update

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -27,16 +27,16 @@ def get_users():
 
 @app.route('/status')
 def status():
-    return (jsonify({"status": "OK"}))
+    return jsonify({"status": "OK"})
 
 
 @app.route('/users/<username>')
 def get_user(username):
     user = users.get(username)
     if user:
-        return (jsonify(user))
+        return jsonify(user)
     else:
-        return (jsonify({"error": "User not found"}), 404)
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
@@ -44,12 +44,15 @@ def add_user():
     data = request.get_json()
     if not data or 'username' not in data:
         return jsonify({"error": "Bad request, 'username' is required"}), 400
+    
     username = data['username']
     if username in users:
         return jsonify({"error": "User already exists"}), 400
-    users[username] = data
-    return (jsonify({"message": "User added successfully",
-                    "user": users[username]}), 201)
+    
+    users[username] = {"name": data["name"], "age": data["age"], "city": data["city"]}
+    
+    return jsonify({"message": "User added successfully",
+                    "user": users[username]}), 201
 
 
 if __name__ == "__main__":
